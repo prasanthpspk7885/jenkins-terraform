fafd06adcfec6991eb9afbeee5cfe91e50d5cd59
prasanth7885 <139453638+prasanthpspk7885@users.noreply.github.com>
2023-08-22 20:05:10 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,12 @@
+variable "filename" {
+    default = "pets.txt"
+}
+variable "content" {
+    default ="we love pets!"
+}
+variable "prefix" {
+    default = "MR"
+}
+variable "separator" {
+  default = "."
+}
