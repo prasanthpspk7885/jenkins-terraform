73ff007b59ea9994e9204de7e2a4ad85283aa763
prasanth7885 <139453638+prasanthpspk7885@users.noreply.github.com>
2023-08-24 20:35:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
-resource "local_file" "pet" {
-filename = var.filename
-content = var.content
+resource "local_file" "pets" {
+filename = each.value
+content = "WE LOVE PETS"
+for_each = var.filename
 }
-resource "random_pet" "pet" {
-    prefix = "mr"
-    separator = "."
-  
- }
+variable "filename" {
+    type =set(string)
+default = [
+"pets.txt",
+"cats.txt",
+"dogs.txt"
+]
+}
